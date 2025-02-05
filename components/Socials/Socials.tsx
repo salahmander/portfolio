3d0bf144f4fb1039ec51db0e134021dbe26d90ae
salahmander <salahabdo@hotmail.co.uk>
2025-02-05 18:41:56 +0000

    feat: create socials component

---
+++ components/Socials/Socials.tsx
@@ -0,0 +1,44 @@
+import { SocialIcons } from "./Socials.helper";
+
+import localText from "./Socials.text.json";
+
+import { Button } from "@/components/ui/button";
+
+const Socials = () => {
+  const links = localText.socials.links;
+  return (
+    <div className="flex flex-wrap items-center gap-2">
+      <Button className="shadow-none hover:bg-background hover:text-primary border-[0.3px] border-transparent hover:border-b-4 hover:border-primary/30 active:border-b transition-all">
+        {localText.socials.scheduleBtn}
+      </Button>
+      <Button
+        variant="outline"
+        className="active:border-b active:scale-[0.97] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
+        asChild
+      >
+        <a href={"/"} target="_blank">
+          {localText.socials.resume}
+        </a>
+      </Button>
+      {Object.keys(links).map((key: string, index: number) => {
+        const link = links[key as keyof typeof links];
+        return (
+          <Button
+            key={index}
+            size="icon"
+            variant="outline"
+            className="active:border-b active:scale-[0.97] hover:border-[0.2px] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
+            asChild
+          >
+            <a href={link} target="_blank">
+              {SocialIcons[key as keyof typeof SocialIcons]}
+              <span className="sr-only">{`${key} - ${link}`}</span>
+            </a>
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
