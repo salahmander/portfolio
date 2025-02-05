1c1ed9d5c0d4824a1ed3f827d4880226ca28caf0
salahmander <salahabdo@hotmail.co.uk>
2025-02-05 18:17:09 +0000

    feat: create profile overview section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import ProfileOverview from "@/components/ProfileOverview/ProfileOverview";
+
 export default function Page() {
   return (
     <main className="relative w-full lg:h-screen p-0 sm:p-5">
       <div className="w-full h-full rounded-2xl lg:border flex flex-wrap justify-between lg:divide-x">
-        <div className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-scroll"></div>
+        <div className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-scroll">
+          <ProfileOverview />
+        </div>
         <div className="relative w-full max-w-4xl mx-auto lg:h-full lg:w-3/5 p-2 md:p-8 lg:overflow-y-scroll">
           <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-8"></div>
         </div>
---
+++ components/ProfileOverview/ProfileOverview.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import { Rss } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import localText from "./ProfileOverview.text.json";
+
+const ProfileOverview = () => {
+  const skills = localText.profileOverview.skills;
+  return (
+    <section className="w-full flex flex-col lg:min-h-[calc(100vh-7rem)]">
+      <Link href="/">
+        <span className="font-mono text-sm underline">
+          {localText.profileOverview.sitename}
+        </span>
+      </Link>
+      <div className="flex justify-between items-center mt-6">
+        <h1 className="head-text-sm">{localText.profileOverview.name}</h1>
+        <div className="flex items-center gap-2">
+          <Button size="icon" variant="ghost" className="rounded-full" asChild>
+            <Link href="/feed">
+              <Rss size={18} />
+              <span className="sr-only">rss feed</span>
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <h3 className="mt-2 text-lg">
+        {localText.profileOverview.tagline}{" "}
+        <span className="sr-only">tagline</span>
+      </h3>
+      <p className="my-6 max-w-2xl text-foreground/80">
+        {localText.profileOverview.about}
+        <span className="sr-only">bio</span>
+      </p>
+      <div className="hidden md:flex flex-col text-sm space-y-2 rounded max-w-2xl text-foreground/70 my-7">
+        {skills.map((skill) => (
+          <p key={skill.category}>
+            <span className="font-semibold text-primary/90">
+              {skill.category}:
+            </span>{" "}
+            {skill.options.join(", ")}
+          </p>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProfileOverview;
