3fc129535ea4ef3c4dc650ef92073fcdd8724bac
salahmander <salahabdo@hotmail.co.uk>
2025-02-05 17:57:09 +0000

    feat: create skeleton for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+    <main className="relative w-full lg:h-screen p-0 sm:p-5">
+      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap justify-between lg:divide-x">
+        <div className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-scroll"></div>
+        <div className="relative w-full max-w-4xl mx-auto lg:h-full lg:w-3/5 p-2 md:p-8 lg:overflow-y-scroll">
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-8"></div>
+        </div>
+      </div>
+    </main>
   );
 }
