a140e2184cc4a72506adfc638245b579eb789d54
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 23:48:00 +0000

    feat: embed react cal, create a pop component

---
+++ components/ScheduleCallPopup/ScheduleCallPopup.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { getCalApi } from "@calcom/embed-react";
+
+import { Button } from "@/components/ui/button";
+
+import { SocialIcons } from "@/lib/SocialIcons";
+
+const ScheduleCallPopup = () => {
+  useEffect(() => {
+    const initializeCalApi = async () => {
+      const cal = await getCalApi({ namespace: "meeting" });
+      cal("ui", {
+        theme: "light",
+        cssVarsPerTheme: {
+          dark: { "--brand-color": "#000000" },
+          light: { "--brand-color": "#000000" },
+        },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    };
+
+    initializeCalApi();
+  }, []);
+  return (
+    <Button
+      data-cal-namespace="meeting"
+      data-cal-link="salah-abdo/meeting"
+      data-cal-config='{"layout":"month_view"}'
+      className="shadow-none hover:bg-background hover:text-primary border-[0.3px] border-transparent hover:border-b-4 hover:border-primary/30 active:border-b transition-all"
+    >
+      Schedule a call {SocialIcons["mobile"]}
+    </Button>
+  );
+};
+
+export default ScheduleCallPopup;
