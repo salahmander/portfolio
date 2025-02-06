9b5fad5e4a9be0f7eb138b70c31ce74d0901003d
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 16:19:32 +0000

    refactor: clean up Tailwind and JSX structure for better readability and responsiveness
    - Improved spacing and removed redundant classes
    - Used `section` for better semantic structure
    - Optimized repository grid with `grid-cols-1 md:grid-cols-2`
    - Replaced `overflow-y-scroll` with `overflow-y-auto`

--- app/repositories/page.tsx
+++ app/repositories/page.tsx
@@ -5,24 +5,16 @@ import RepositoryCard from "@/components/RepositoryCard/RepositoryCard";
 const Repositories = () => {
   return (
     <main className="relative w-full lg:h-screen p-0 sm:p-5">
-      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap justify-between lg:divide-x">
-        <div className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-scroll">
+      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap lg:divide-x">
+        <section className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
           <ProfileOverview />
-        </div>
-        <div className="relative w-full max-w-4xl mx-auto lg:h-full lg:w-3/5 p-2 md:p-8 lg:overflow-y-scroll">
+        </section>
+        <section className="relative w-full lg:w-3/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
           <NavigationBar />
-          <div className="my-8 w-full max-w-3xl md:grid grid-cols-2 gap-3">
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
+          <div className="my-8 grid grid-cols-1 md:grid-cols-2 gap-3">
+            {Array(9).fill(<RepositoryCard />)}
           </div>
-        </div>
+        </section>
       </div>
     </main>
   );
