d0447df82cb8927c4f6def9df2d8b980f5a36b07
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 22:16:51 +0000

    fix: fix button placement on smaller devices in socials component

--- components/Socials/Socials.tsx
+++ components/Socials/Socials.tsx
@@ -14,32 +14,34 @@ const Socials = () => {
       <Button className="shadow-none hover:bg-background hover:text-primary border-[0.3px] border-transparent hover:border-b-4 hover:border-primary/30 active:border-b transition-all">
         {localText.socials.contactBtn} {SocialIcons["mail"]}
       </Button>
-      <Button
-        variant="outline"
-        className="active:border-b active:scale-[0.97] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
-        asChild
-      >
-        <a href={"/"} target="_blank">
-          {localText.socials.resume}
-        </a>
-      </Button>
-      {Object.keys(links).map((key: string, index: number) => {
-        const link = links[key as keyof typeof links];
-        return (
-          <Button
-            key={index}
-            size="icon"
-            variant="outline"
-            className="active:border-b active:scale-[0.97] hover:border-[0.2px] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
-            asChild
-          >
-            <a href={link} target="_blank">
-              {SocialIcons[key as keyof typeof SocialIcons]}
-              <span className="sr-only">{`${key} - ${link}`}</span>
-            </a>
-          </Button>
-        );
-      })}
+      <div className="flex items-center gap-2">
+        <Button
+          variant="outline"
+          className="active:border-b active:scale-[0.97] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
+          asChild
+        >
+          <a href={"/"} target="_blank">
+            {localText.socials.resume}
+          </a>
+        </Button>
+        {Object.keys(links).map((key: string, index: number) => {
+          const link = links[key as keyof typeof links];
+          return (
+            <Button
+              key={index}
+              size="icon"
+              variant="outline"
+              className="active:border-b active:scale-[0.97] hover:border-[0.2px] hover:border-b-4 hover:border-primary/30 hover:bg-background shadow-none transition-all duration-100"
+              asChild
+            >
+              <a href={link} target="_blank">
+                {SocialIcons[key as keyof typeof SocialIcons]}
+                <span className="sr-only">{`${key} - ${link}`}</span>
+              </a>
+            </Button>
+          );
+        })}
+      </div>
     </div>
   );
 };
