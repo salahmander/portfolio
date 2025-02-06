b1df1c1ebca4a71cdcd42eb45321a25d4dd79aa4
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 20:32:45 +0000

    fix: fix height issue on smaller screens for loading, and error page

--- app/error.tsx
+++ app/error.tsx
@@ -10,7 +10,7 @@ type ErrorPageProps = {
 const ErrorPage = ({ error }: ErrorPageProps) => {
   console.error(error);
   return (
-    <main className="relative w-full lg:h-screen p-0 sm:p-5">
+    <main className="relative w-full h-screen p-5">
       <div className="w-full h-full border rounded-2xl flex justify-center items-center text-black">
         <h1 className="text-5xl font-bold mb-4">Something Went Wrong!</h1>
         <p className="text-xl mb-8">{error.toString()}</p>
--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@
 
 const LoadingPage = () => {
   return (
-    <main className="relative w-full lg:h-screen p-0 sm:p-5">
+    <main className="relative w-full h-screen p-5">
       <div className="w-full h-full border rounded-2xl flex justify-center items-center text-black">
         <h1 className="text-5xl font-bold mb-8 animate-pulse">Loading...</h1>
         <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-black"></div>
