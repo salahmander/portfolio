83360b1e49ffa8ab2aeb56f83dc6fc1a30ff0410
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 16:59:28 +0000

    refactor: improve Tailwind structure for better readability and responsiveness
    - Used `section` elements for better semantic structure
    - Switched `space-y-5` to `space-y-4` for better layout
    - Replaced `overflow-y-scroll` with `overflow-y-auto` to avoid unnecessary scrollbars

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,19 +5,16 @@ import ProjectCard from "@/components/ProjectCard/ProjectCard";
 const Projects = () => {
   return (
     <main className="relative w-full lg:h-screen p-0 sm:p-5">
-      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap justify-between lg:divide-x">
-        <div className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-scroll">
+      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap lg:divide-x">
+        <section className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
           <ProfileOverview />
-        </div>
-        <div className="relative w-full max-w-4xl mx-auto lg:h-full lg:w-3/5 p-2 md:p-8 lg:overflow-y-scroll">
+        </section>
+        <section className="flex flex-col items-center relative w-full mx-auto lg:w-3/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
           <NavigationBar />
-          <div className="my-8 space-y-5">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+          <div className="my-8 space-y-4 max-w-4xl">
+            {Array(4).fill(<ProjectCard />)}
           </div>
-        </div>
+        </section>
       </div>
     </main>
   );
