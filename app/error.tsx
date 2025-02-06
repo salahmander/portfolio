3d236ac5afe3957169834fb9e12b8e1524cf3283
salahmander <salahabdo@hotmail.co.uk>
2025-02-06 19:16:40 +0000

    feat: create minimalist error page

---
+++ app/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+type ErrorPageProps = {
+  error: Error;
+};
+
+const ErrorPage = ({ error }: ErrorPageProps) => {
+  console.error(error);
+  return (
+    <main className="relative w-full lg:h-screen p-0 sm:p-5">
+      <div className="w-full h-full border rounded-2xl flex justify-center items-center text-black">
+        <h1 className="text-5xl font-bold mb-4">Something Went Wrong!</h1>
+        <p className="text-xl mb-8">{error.toString()}</p>
+        <Button
+          asChild
+          variant="outline"
+          className="border-black text-black hover:bg-black hover:text-white transition-colors"
+        >
+          <Link href="/">Return Home</Link>
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default ErrorPage;
