ddf475363d0730e4dd92f8e32600c78c3070a4a2
salahmander <salahabdo@hotmail.co.uk>
2025-02-08 01:37:47 +0000

    feat: get all my current public repositories and populate page

--- app/repositories/page.tsx
+++ app/repositories/page.tsx
@@ -2,7 +2,10 @@ import NavigationBar from "@/components/NavigationBar/NavigationBar";
 import ProfileOverview from "@/components/ProfileOverview/ProfileOverview";
 import RepositoryCard from "@/components/RepositoryCard/RepositoryCard";
 
-const RepositoriesPage = () => {
+import getRepositories from "@/app/actions/getRepositories";
+
+const RepositoriesPage = async () => {
+  const repositories = await getRepositories();
   return (
     <main className="relative w-full lg:h-screen p-0 sm:p-5">
       <div className="w-full h-full rounded-2xl lg:border flex flex-wrap lg:divide-x">
@@ -12,7 +15,9 @@ const RepositoriesPage = () => {
         <section className="flex flex-col items-center relative w-full lg:w-3/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
           <NavigationBar />
           <div className="my-8 grid grid-cols-1 md:grid-cols-3 gap-3">
-            {Array(9).fill(<RepositoryCard />)}
+            {repositories.map((repository) => (
+              <RepositoryCard key={repository.id} repository={repository} />
+            ))}
           </div>
         </section>
       </div>
