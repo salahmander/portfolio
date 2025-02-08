3f6491597a58ed90658a2d29e33145ac777ceedf
salahmander <salahabdo@hotmail.co.uk>
2025-02-08 01:36:02 +0000

    refactor: update repository card to dynamically populate content via prop

--- components/RepositoryCard/RepositoryCard.tsx
+++ components/RepositoryCard/RepositoryCard.tsx
@@ -1,23 +1,40 @@
 import { Star } from "lucide-react";
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
 
-const RepositoryCard = () => {
+type RepositoryCardProps = {
+  repository: RestEndpointMethodTypes["repos"]["listForUser"]["response"]["data"][number];
+};
+
+const RepositoryCard = ({ repository }: RepositoryCardProps) => {
+  const { forks, stargazers_count } = repository;
+
+  const hasForks = Boolean(forks && forks > 0);
+  const hasStar = Boolean(stargazers_count && stargazers_count > 0);
+
   return (
     <div className="w-full min-h-[90px] rounded-lg p-2 border text-sm transition-all duration-100 hover:bg-muted/50">
-      <a href="/" target="_blank" rel="noopener noreferrer" className="block">
+      <a
+        href={repository.html_url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block"
+      >
         <div className="flex flex-col gap-1">
           <div className="flex items-center gap-2">
-            <h2 className="text-base font-medium">Lorem, ipsum dolor.</h2>
-            <p className="flex items-center gap-1 text-foreground ml-auto">
-              <Star size={12} /> 2
-            </p>
+            <h2 className="text-base font-medium">{repository.name}</h2>
+            {hasStar && (
+              <p className="flex items-center gap-1 text-foreground ml-auto">
+                <Star size={12} />
+                {stargazers_count}
+              </p>
+            )}
           </div>
           <p className="text-xs text-muted-foreground">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-            optio architecto? Tempora quibusdam commodi facilis?
+            {repository.description}
           </p>
           <div className="flex items-center gap-2 text-xs text-muted-foreground pt-2">
-            <p>TypeScript</p>
-            <p className="ml-auto">2 forks</p>
+            <p>{repository.language}</p>
+            {hasForks && <p className="ml-auto">{forks} forks</p>}
           </div>
         </div>
       </a>
