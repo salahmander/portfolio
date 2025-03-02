b180d8b4bba5f4d76a4be048d109c4c8bdcfe1c3
salahmander <salahabdo@hotmail.co.uk>
2025-03-02 19:40:52 +0000

    feat: create loading page for home page, rendering skeleton job card

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,21 +1,27 @@
-"use client";
+import ProfileOverview from "@/components/ProfileOverview/ProfileOverview";
 
-/**
- * LoadingPage component renders a full-screen loading indicator.
- *
- * @returns {JSX.Element} A React component that displays a loading message and a spinning animation.
- *
- * @component
- */
-const LoadingPage = () => {
+import NavigationBar from "@/components/NavigationBar/NavigationBar";
+
+import JobCardSkeleton from "@/components/JobCard/Skeleton/JobCardSkeleton";
+
+const HomeLoadingPage = async () => {
   return (
-    <main className="relative w-full h-screen p-5">
-      <div className="w-full h-full border rounded-2xl flex flex-col justify-center items-center text-black">
-        <h1 className="text-5xl font-bold mb-8 animate-pulse">Loading...</h1>
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-black"></div>
+    <main className="relative w-full lg:h-screen p-0 sm:p-5">
+      <div className="w-full h-full rounded-2xl lg:border flex flex-wrap lg:divide-x">
+        <section className="w-full lg:w-2/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
+          <ProfileOverview />
+        </section>
+        <section className="flex flex-col items-center relative w-full lg:w-3/5 p-2 md:p-8 lg:h-full lg:overflow-y-auto">
+          <NavigationBar />
+          <div className="my-8 w-full self-start max-w-xl space-y-10">
+            {[...Array(3)].map((_, index) => (
+              <JobCardSkeleton key={index} />
+            ))}
+          </div>
+        </section>
       </div>
     </main>
   );
 };
 
-export default LoadingPage;
+export default HomeLoadingPage;
