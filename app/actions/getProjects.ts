86c966e058e671e8859af0033051b7ecc75a61c0
salahmander <salahabdo@hotmail.co.uk>
2025-03-02 16:14:09 +0000

    feat: create new server action to fetch project

---
+++ app/actions/getProjects.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+export const getProjects = async () => {
+  const databaseId = "199fb82e047b804aa8e0cc1d4b5592d8";
+  try {
+    const projects = await notion.databases.query({
+      database_id: databaseId,
+    });
+    
+    const formattedProjects = projects.results.map((project) => ({
+      name: project.properties.name?.rich_text[0]?.plain_text,
+      description: project.properties.description?.rich_text[0]?.plain_text,
+      githubUrl: project.properties.github_url?.rich_text[0]?.plain_text,
+      images: {
+        light: project.properties.light_image_url?.rich_text[0]?.plain_text,
+        dark: project.properties.dark_image_url?.rich_text[0]?.plain_text,
+      },
+      webUrl: project.properties.web_url?.rich_text[0]?.plain_text,
+      skills: project.properties.skills?.multi_select,
+      createdAt: project.properties?.created_at?.date?.start,
+      isArchived: project.properties?.is_archived.checkbox,
+    }));
+
+    return formattedProjects;
+  } catch (error) {
+    console.error("Error fetching projects from Notion:", error);
+    return [];
+  }
+};
